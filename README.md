ae2e009d1d9f8c8c475317e88c24a7f68bc40556
Christoph Schmied <smith1401@hotmail.com>
2022-04-04 12:30:21 +0200

    included license; update of REAMDE.md

--- README.md
+++ README.md
@@ -1,44 +1,94 @@
-# PartyPhone
+# Arduino Rotary phone for "offline" parties
 
 ## Foreword
-Who doesn't know it, you are at a party and everyone is checking their phones or swiping through their instagram. I find it very annoying when people stop communicating at instead dive into the digital world. 
+Who doesn't know it, you are at a party and everyone is checking their phones or swiping through their Instagram. I find it very annoying when people stop communicating and instead dive into the digital world.
 
-Somehow I have always felt jealous of people growing up before the digital age. Don't get me wrong I really enjoy that tinkering and developing new things is very easy to do nowadays, but sometimes we seem to forget that our smartphone is not our best fried 😅
+Somehow I have always felt jealous of people growing up before the digital age. Don't get me wrong, I really enjoy that tinkering and developing new things is very easy to do nowadays, but sometimes we seem to forget that our smartphone is **not** our best friend 😅
 
-OK, enough of me rambling on. You are here for an electronics project!
+OK, enough of me rambling on. Let's briefly talk about how it all started and then get into the details.
 
 ## Project idea
-When my girlfriend's grandma tidied up her attic, she found an old rotary phone they used back in the 80s. As my girlfriend knows me very well she saved it from the trash and gave it to me saying "I know you love this old electronics stuff ..." (she really knows me well 😄).
-In this moment the idea was born to convert the old rotary phone's internals to a cell phone (bring internal to the 21st century). One thing I always wanted to preserve were the looks and feels of an old rotary phone though. In the beginning this was just an idea but I really did not have an use-case for it.
+When my girlfriend's grandma tidied up her attic, she found an old rotary phone they used back in the 80s (it seems like all phones here in Austria were olive green back in the days 🤨). As my girlfriend knows me very well, she saved it from the trash and gave it to me saying, "I know you love this old electronics stuff ..." (she really knows me well 😄).
+In this moment the idea was born to convert the old rotary phone and bring it's internals to the 21st century. What I always wanted to preserve were the looks and feels of an old rotary phone though. In the beginning this was just an idea but I really did not have a use-case for it.
 
-The whole thing started to take shape when I was at a party where exatly those things described in the foreword happened and I thought to myself, 'How cool would it be if there were no smartphones at all and maybe just an old regular phone (a rotary phone) for emergencies and ordering a pizza for example'. Then immediately the old rotary phone of my girlfriend's grandparents popped into my mind 🙂
+![Old austrian rotary phone](docs/rotary_phone.jpg)
 
-Then the tinkering began!
+The whole thing started to take shape when I was at a party where exactly the things described before happened and I thought to myself, 'How cool would it be if there were no smartphones at all and maybe just an old regular phone (a rotary phone) for emergencies or ordering a pizza'. Then immediately the old rotary phone of my girlfriend's grandparents popped into my mind 🙂
 
 ## Research
-As I work in research, the first thing I always do is, well researching 😃 Someone must have had the same idea before or at least something similiar. It is always good to use things someone has already wrapped their head around. And sure enough, there was something almost as I needed. [Bluetooth Portable Rotary Phone](https://www.sparkfun.com/products/retired/8929) by Sparkfun, altough discontinued.
+As I work in research, the first thing I always do is, well, research 😃 Someone must have had the same idea before or must have thought of at least something similiar. It is always good to use things someone has had already wrapped their head around. And sure enough, I found something. Sparkfun had a product called [Bluetooth Portable Rotary Phone](https://www.sparkfun.com/products/retired/8929) in their portfolio a few years ago. Unfortunately, it got retired. Luckily, they still provide all the details about the circuit they used, as well as information about the rotary dial, the ringer circuit, you name it :) A huge thank you to Sparkfun for providing all this information for free.
+
 
 ## Hardware
+With research out of the way, I started to think about the components I would need for such a project. As I wanted it to be quick and simple, I mostly used modules instead of developing my own PCB. So really any hobbyist can build it. The hardware components I used are as follows
+
 * Rotary phone
-* SIM800(LC)
-* LN293N
+* SIM800(LC) (2G Cellular phone module)
+* LN293N (H-bridge)
 * Arduino (Pro mini, nano, etc.)
-* Voltage boost module
+* Voltage boost module (XL6009, SX1308, etc.)
 * Li-Po (Li-on) battery
-* Li-Po charger
+* Li-Po charger module (TP4056)
 * USB-Serial Converter (optional)
 
-## Firmware
-...
+Those are the essential components. Of course you will need some more passive components like resistors and capacitors, but I assume anyone trying to build this project will have some laying around.
 
-## Contributing
-State if you are open to contributions and what your requirements are for accepting them.
-For people who want to make changes to your project, it's helpful to have some documentation on how to get started. Perhaps there is a script that they should run or some environment variables that they need to set. Make these steps explicit. These instructions could also be useful to your future self.
-You can also document commands to lint the code or run tests. These steps help to ensure high code quality and reduce the likelihood that the changes inadvertently break something. Having instructions for running tests is especially helpful if it requires external setup, such as starting a Selenium server for testing in a browser.
+## Schematic
+In the schematic you can see that I mostly used PCB modules to keep things simple. The only custom built section is the part where the microcontroller interfaces with the H-bridge. It has all been layed out on a perfboard with connectors for the individual modules.
+
+![schematic](docs/schematic.png)
+## Firmware
+The firmware has been developed in Arduino C++, again to keep things simple for everyone. The code is by no means optimized or anything and I know C runs a lot quicker on those tiny 8-bit microcontrollers. I personally find the Arduino enviroment very handy for quick prototyping and so on, as a lot of useful libraries exist.
+
+The main firmware structure is oriented towards a finite-state-machine approach. I found a very nice [tutorial](https://www.youtube.com/watch?v=cZ2rHqBXO1s&t=368s) by a YouTube channel called [Playful Technology](https://www.youtube.com/c/PlayfulTechnology/featured). He uses an old rotary phone as a prop in an Escape Room game. Definitely worth a watch 🙂!
+
+For an overview I created a state diagram of the main program flow (I tried to use [mermaid](https://github.com/mermaid-js/mermaid) for the first time, this is why it looks a bit messy 😄). 
+
+![state-diagram](docs/state_diagram.png)
+
+The main Arduino ```loop()``` consists of 4 simple update routines which check the switches, update timers, receive data from the SIM800 module and update the state machine.
+
+```cpp
+void loop()
+{
+  updateSwitches();
+  updateTickers();
+  updateSIM800();
+  updateStateMachine();
+}
+```
+
+Altough the code is by no means optimized, the main loop runs with an update rate of > 10 kHz, which I think is more than enough. Thanks again to Playful Technology for leading my to a finite-state-machine approach. I also found a nice tutorial regarding FDMs on [arduinoplusplus](https://arduinoplusplus.wordpress.com/2019/07/06/finite-state-machine-programming-basics-part-1/) if you are interested.
 
 ## Acknowledgment
-Show your appreciation to those who have contributed to the project.
+Many thanks also to 
+
+- *Michael Contreras* for providing a very easy to use [arduino-timer](https://github.com/contrem/arduino-timer) library
+- *Thomas Ouellet Fredericks* for providing a very cool debouncing library for switches [Bounce2](https://github.com/thomasfredericks/Bounce2)
+- Guys at *Sparkfun* for providing a great deal of information about rotary phones and how a conversion can be done [Bluetooth Portable Rotary Phone](https://www.sparkfun.com/products/retired/9803)
+- [*Playful Technology*](https://www.youtube.com/c/PlayfulTechnology/featured) for providing a very detailed tutorial on how to convert an old rotary phone
 
 ## License
-For open source projects, say how it is licensed.
+MIT License
+
+Copyright (c) 2022 Christoph Schmied
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
+/*
+  MIT License
+
+  Copyright (c) 2022 Christoph Schmied
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 #include <Arduino.h>
 #include <Bounce2.h>
 #include <SoftwareSerial.h>
 #include <arduino-timer.h>
+#include "sim800_defines.h"
 
 #define DEBOUNCE_INTERVAL_MS 5
 #define START_CALL_DELAY_MS 4000
@@ -11,24 +36,19 @@
 #define RINGER_PULSE_MS 25
 #define RINGER_PIN_COUNT 2
 #define NUM_RINGS 1
-
 #define MAX_NUMBER_DIGITS 15
-#define SIM800_POLL_TIMOUT_MS 200
-#define AT_CMD_BUFF_LEN 64
 #define LOCAL_COUNTRY_CODE "+43"
 
-const uint8_t call_pin = 2;
 const uint8_t hook_pin = 3;
 const uint8_t dial_pin = 5;
 const uint8_t number_pin = 6;
 const uint8_t ringer_pins[] = {10, 11};
 
-Bounce callSwitch = Bounce(call_pin, INPUT);
 Bounce hookSwitch = Bounce(hook_pin, INPUT);
 Bounce dialSwitch = Bounce(dial_pin, INPUT);
 Bounce numberSwitch = Bounce(number_pin, INPUT);
 
-SoftwareSerial ss(2, 7);
+SoftwareSerial ss(SIM800_RX_PIN, SIM800_TX_PIN);
 
 #define DEBUG Serial
 #define SIM800 ss
@@ -60,15 +80,117 @@ bool incomingCall = false;
 State state = State::Idle;
 uint8_t ringCount = 0;
 
-char sim800Buffer[AT_CMD_BUFF_LEN] = {0x00};
+// Buffers
+char sim800Buffer[SIM800_AT_CMD_BUFF_LEN] = {0x00};
 char *pSIM800 = &sim800Buffer[0];
-
 char internationalNumberBuffer[MAX_NUMBER_DIGITS + 1];
 
-const char* convertNumberToCountryCode(const char * num)
+// Prototypes
+const char *convertNumberToCountryCode(const char *num);
+bool ring(void *);
+void startRinging();
+void callAnswered();
+void parseSIM800response();
+void receiveSIM800(bool debug_out = false);
+void pollSIM800(bool debug_out = false);
+void updateSwitches();
+void updateTickers();
+void updateSIM800();
+void updateStateMachine();
+
+void setup()
+{
+  // Setup switch debounce intervals
+  hookSwitch.interval(DEBOUNCE_INTERVAL_MS);
+  dialSwitch.interval(DEBOUNCE_INTERVAL_MS);
+  numberSwitch.interval(DEBOUNCE_INTERVAL_MS);
+
+  // Configure initial state of ringer pins (must be different)
+  for (size_t i = 0; i < RINGER_PIN_COUNT; i++)
+  {
+    pinMode(ringer_pins[i], OUTPUT);
+  }
+
+  digitalWrite(ringer_pins[0], HIGH);
+  digitalWrite(ringer_pins[1], LOW);
+
+  // Setup hardware and software serial ocmmunication
+  SIM800.begin(57600);
+  DEBUG.begin(57600);
+
+  // If the hook switch is LOW during startup, switch so serial-ping-pong mode
+  if (hookSwitch.read() == LOW)
+  {
+    DEBUG.println(F("Changing to Serial mode."));
+
+    while (hookSwitch.read() == LOW)
+    {
+      while (DEBUG.available())
+        SIM800.write(DEBUG.read());
+
+      while (SIM800.available())
+        DEBUG.write(SIM800.read());
+
+      hookSwitch.update();
+    }
+
+    DEBUG.println(F("Exit Serial mode."));
+  }
+
+  // Initialize SIM800 module and get basic information
+  SIM800.println(SIM800_HANDSHAKE_CMD); // Once the handshake test is successful, it will back to OK
+  pollSIM800();
+  SIM800.println(SIM800_SIGNAL_QUALITY_CMD); // Signal quality test, value range is 0-31 , 31 is the best
+  pollSIM800(true);
+  SIM800.println(SIM800_SIM_INFO_CMD); // Read SIM information to confirm whether the SIM is plugged
+  pollSIM800(true);
+  SIM800.println(SIM800_REGISTERED_CMD); // Check whether it has registered in the network
+  pollSIM800(true);
+  SIM800.println(SIM800_BATTERY_STATUS_CMD);
+  pollSIM800(true);
+  SIM800.println(SIM800_DISABLE_RINGER_CMD);
+  pollSIM800();
+  SIM800.println(SIM800_MIC_GAIN_CMD(2));
+  pollSIM800(true);
+
+  DEBUG.println(F("ready"));
+}
+
+State prevState = State::Idle;
+uint32_t prevMillis = millis();
+uint32_t duration = 0;
+int loopCount = 0;
+
+void loop()
+{
+  uint32_t start = micros();
+
+  updateSwitches();
+  updateTickers();
+  updateSIM800();
+  updateStateMachine();
+
+  uint32_t stop = micros();
+  duration += (stop - start);
+  loopCount++;
+
+  // Averge rate is above 10kHz - fast enough!
+  // if (millis() - prevMillis > 1000)
+  // {
+  //   int avg = duration / loopCount;
+  //   DEBUG.print(state);
+  //   DEBUG.print(F("  "));
+  //   DEBUG.print(avg);
+  //   DEBUG.println(F(" us"));
+  //   prevMillis = millis();
+  //   duration = loopCount = 0;
+  // }
+}
+
+const char *convertNumberToCountryCode(const char *num)
 {
   memset(internationalNumberBuffer, 0, MAX_NUMBER_DIGITS + 1);
-  
+
   // Is it an international number
   if (strncmp(num, "00", 2) == 0)
   {
@@ -91,6 +213,7 @@ const char* convertNumberToCountryCode(const char * num)
 
 bool ring(void *)
 {
+  // Switch the polarity of the ringer pins
   for (size_t i = 0; i < RINGER_PIN_COUNT; i++)
   {
     digitalWrite(ringer_pins[i], !digitalRead(ringer_pins[i]));
@@ -106,7 +229,7 @@ void startRinging()
 
   // Attach ticker for ringer on/off period
   ringerTask = timer.in(RINGER_ON_MS, [](void *) -> bool
-  { 
+                        { 
     // Cancel pinChangeTask and start new ringerTask
     timer.cancel(pinChangeTask);
     ringerTask = timer.in(RINGER_OFF_MS, [](void *) -> bool
@@ -119,8 +242,7 @@ void startRinging()
         return false;
     });
 
-    return false; 
-  });
+    return false; });
 }
 
 void callAnswered()
@@ -145,36 +267,36 @@ void parseSIM800response()
   */
 
   // Check if someone is calling
-  char *found = strstr(sim800Buffer, "RING");
+  char *found = strstr_P(sim800Buffer, (const char *)SIM800_RESP_RING);
   if (found != NULL && state == State::Idle)
   {
     incomingCall = true;
   }
   else if (found != NULL && state != State::Idle)
   {
-    SIM800.println(F("ATH"));
+    SIM800.println(SIM800_ANSWER_CALL_CMD);
   }
 
-  found = strstr(sim800Buffer, "NO CARRIER");
+  found = strstr_P(sim800Buffer, (const char *)SIM800_RESP_NO_CARRIER);
   if (found != NULL && (state == State::Ringing || state == State::Connected))
   {
     timer.cancel();
     state = State::Idle;
     DEBUG.println(F("Other end hung up!"));
-    SIM800.println(F("AT+STTONE=1,5,15300000"));
+    SIM800.println(SIM800_HUNG_UP_TONE);
     incomingCall = false;
   }
 
-  found = strstr(sim800Buffer, "BUSY");
+  found = strstr_P(sim800Buffer, (const char *)SIM800_RESP_BUSY);
   if (found != NULL)
   {
     state = State::Engaged;
     DEBUG.println(F("Other end is busy!"));
-    SIM800.println(F("AT+SIMTONE=1,425,500,500,300000"));
+    SIM800.println(SIM800_BUSY_TONE);
   }
 }
 
-void receiveSIM800()
+void receiveSIM800(bool debug_out)
 {
   int count = 0;
   while (SIM800.available())
@@ -191,22 +313,26 @@ void receiveSIM800()
   {
     *pSIM800 = '\0';
     pSIM800 = &sim800Buffer[0];
-    // DEBUG.println(sim800Buffer);
     parseSIM800response();
+
+    if (debug_out)
+      DEBUG.println(sim800Buffer);
+
     strcpy(sim800Buffer, "");
   }
 }
 
-void pollSIM800()
+void pollSIM800(bool debug_out)
 {
   uint32_t startPoll = millis();
-  while (!SIM800.available() && (millis() - startPoll) < SIM800_POLL_TIMOUT_MS) {}
-  receiveSIM800();
+  while (!SIM800.available() && (millis() - startPoll) < SIM800_POLL_TIMOUT_MS)
+  {
+  }
+  receiveSIM800(debug_out);
 }
 
 void updateSwitches()
 {
-  callSwitch.update();
   hookSwitch.update();
   dialSwitch.update();
   numberSwitch.update();
@@ -234,8 +360,8 @@ void updateStateMachine()
     strcpy(dialedNumber, "");
     currentDigit = ringCount = pulseCount = 0;
     timer.cancel();
-    SIM800.println(F("AT+STTONE=0"));
-    SIM800.println(F("ATH"));
+    SIM800.println(SIM800_TONE_STOP);
+    SIM800.println(SIM800_HANGUP_CALL_CMD);
   }
 
   switch (state)
@@ -246,13 +372,11 @@ void updateStateMachine()
     if (hookSwitch.fell())
     {
       state = State::Dialtone;
-      // state = State::Ringing;
-      SIM800.println(F("AT+STTONE=1,1,15300000"));
+      SIM800.println(SIM800_DIAL_TONE);
       DEBUG.println(F("Dialtone -> beeeeeep"));
     }
 
     // If a call is received
-    // if (callSwitch.fell() && hookSwitch.read() == HIGH)
     if (incomingCall)
     {
       state = State::Ringing;
@@ -267,7 +391,7 @@ void updateStateMachine()
     if (dialSwitch.fell())
     {
       state = State::Dialling;
-      SIM800.println(F("AT+STTONE=0"));
+      SIM800.println(SIM800_TONE_STOP);
       DEBUG.println(F("Started dialling"));
     }
   }
@@ -276,37 +400,38 @@ void updateStateMachine()
   case State::Dialling:
   {
     // If a pulse has been detected
-      if (numberSwitch.rose())
+    if (numberSwitch.rose())
+    {
+      pulseCount++;
+    }
+
+    // If the dial has returned to its initial position
+    if (dialSwitch.rose())
+    {
+      // Start new task to wait for no input in order to start a call
+      timer.cancel(startCallTask);
+      startCallTask = timer.in(START_CALL_DELAY_MS, [](void *)
+                               { state = State::Connecting; return false; });
+
+      // Zero == 10 pulses
+      if (pulseCount == 10)
+        pulseCount = 0;
+
+      // Add current digit dialed to number
+      dialedNumber[currentDigit++] = (char)((char)pulseCount + '0');
+      dialedNumber[currentDigit] = '\0';
+      DEBUG.print(F("\rNumber: "));
+      DEBUG.print(dialedNumber);
+
+      if (strlen(dialedNumber) > MAX_NUMBER_DIGITS)
       {
-        pulseCount++;
+        state = State::InvalidNumber;
       }
-
-      // If the dial has returned to its initial position
-      if (dialSwitch.rose())
+      else
       {
-        // Start new task to wait for no input in order to start a call
-        timer.cancel(startCallTask);
-        startCallTask = timer.in(START_CALL_DELAY_MS, [](void *){ state = State::Connecting; return false;});
-
-        // Zero == 10 pulses
-        if (pulseCount == 10)
-          pulseCount = 0;
-
-        // Add current digit dialed to number
-        dialedNumber[currentDigit++] = (char)((char)pulseCount + '0');
-        dialedNumber[currentDigit] = '\0';
-        DEBUG.print(F("\rNumber: "));
-        DEBUG.print(dialedNumber);
-
-        if (strlen(dialedNumber) > MAX_NUMBER_DIGITS)
-        {
-          state = State::InvalidNumber;
-        }
-        else
-        {
-          pulseCount = 0;
-        }
+        pulseCount = 0;
       }
+    }
   }
   break;
 
@@ -316,10 +441,10 @@ void updateStateMachine()
     const char *number = convertNumberToCountryCode(dialedNumber);
     DEBUG.print(F("\r\nConnecting to "));
     DEBUG.println(number);
-    SIM800.print(F("ATD+ "));
+    SIM800.print(SIM800_DIAL_NUMBER_CMD);
     SIM800.print(number);
     SIM800.println(F(";"));
-    SIM800.println(F("AT+SIMTONE=1,425,1000,4000,60000")); // Ringing tone
+    SIM800.println(SIM800_RINGING_TONE); // Ringing tone
 
     pollSIM800();
     state = State::Connected;
@@ -327,8 +452,8 @@ void updateStateMachine()
   break;
 
   case State::Engaged:
-    state = State::Idle;
     DEBUG.println(F("Engaged!"));
+    state = State::Idle;
     break;
 
   case State::InvalidNumber:
@@ -350,7 +475,7 @@ void updateStateMachine()
     if (hookSwitch.fell())
     {
       state = State::Connected;
-      SIM800.println(F("ATA"));
+      SIM800.println(SIM800_ANSWER_CALL_CMD);
       DEBUG.println(F("Call answered!"));
       incomingCall = false;
     }
@@ -364,100 +489,4 @@ void updateStateMachine()
   }
   break;
   }
-}
-
-void setup()
-{
-  callSwitch.interval(DEBOUNCE_INTERVAL_MS);
-  hookSwitch.interval(DEBOUNCE_INTERVAL_MS);
-  dialSwitch.interval(DEBOUNCE_INTERVAL_MS);
-  numberSwitch.interval(DEBOUNCE_INTERVAL_MS);
-
-  // attachInterrupt(digitalPinToInterrupt(hook_pin), callAnswered, RISING);
-  // attachInterrupt(digitalPinToInterrupt(call_pin), [](){ incomingCall = true; }, FALLING);
-
-  for (size_t i = 0; i < RINGER_PIN_COUNT; i++)
-  {
-    pinMode(ringer_pins[i], OUTPUT);
-  }
-
-  // Initial state
-  digitalWrite(ringer_pins[0], HIGH);
-  digitalWrite(ringer_pins[1], LOW);
-
-  SIM800.begin(57600);
-  DEBUG.begin(57600);
-
-  if (hookSwitch.read() == LOW)
-  {
-    DEBUG.println(F("Changing to Serial mode."));
-
-    while (hookSwitch.read() == LOW)
-    {
-      while (DEBUG.available())
-        SIM800.write(DEBUG.read());
-
-      while (SIM800.available())
-        DEBUG.write(SIM800.read());
-
-      hookSwitch.update();
-    }
-
-    DEBUG.println(F("Exit Serial mode."));
-
-  }
-
-  // DEBUG.println(__FILE__);
-  // DEBUG.println(__DATE__);
-  // DEBUG.println(__TIME__);
-
-  SIM800.println(F("AT")); //Once the handshake test is successful, it will back to OK
-  pollSIM800();
-  SIM800.println(F("AT+CSQ")); //Signal quality test, value range is 0-31 , 31 is the best
-  pollSIM800();
-  SIM800.println(F("AT+CCID")); //Read SIM information to confirm whether the SIM is plugged
-  pollSIM800();
-  SIM800.println(F("AT+CREG?")); //Check whether it has registered in the network
-  pollSIM800();
-  SIM800.println(F("AT+CBC"));
-  pollSIM800();
-
-  SIM800.println(F("AT+CALS=0,0"));
-  pollSIM800();
-
-  SIM800.println(F("AT+CMIC=0,2"));
-  pollSIM800();
-  
-
-  DEBUG.println(F("ready"));
-}
-
-State prevState = State::Idle;
-uint32_t prevMillis = millis();
-uint32_t duration = 0;
-int loopCount = 0;
-
-void loop()
-{
-  uint32_t start = micros();
-
-  updateSwitches();
-  updateTickers();
-  updateSIM800();
-  updateStateMachine();
-
-  uint32_t stop = micros();
-  duration += (stop - start);
-  loopCount++;
-
-  // if (millis() - prevMillis > 1000)
-  // {
-  //   int avg = duration / loopCount;
-  //   DEBUG.print(state);
-  //   DEBUG.print(F("  "));
-  //   DEBUG.print(avg);
-  //   DEBUG.println(F(" us"));
-  //   prevMillis = millis();
-  //   duration = loopCount = 0;
-  // }
 }
